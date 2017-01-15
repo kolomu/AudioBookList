96ed46aba99c11fb4deeb51d00ff29fa771aeae3
kolomu <kevin-olomu@web.de>
2017-01-15 19:26:00 +0100

    formatted whitespace

--- AudioBookList/src/java/handler/EventHandlerBase.java
+++ AudioBookList/src/java/handler/EventHandlerBase.java
@@ -9,21 +9,23 @@ import javax.servlet.http.HttpServletResponse;
 
 public abstract class EventHandlerBase {
 
-	protected abstract String getURL();
+    protected abstract String getURL();
 
-	public void process(ServletContext sc, HttpServletRequest request, HttpServletResponse response) throws Exception,ServletException,IOException {
-		System.out.println("forward: Using default process");
-	}
+    public void process(ServletContext sc, HttpServletRequest request, HttpServletResponse response) throws Exception, ServletException, IOException {
+        System.out.println("forward: Using default process");
+    }
 
-	public void forward(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException {
-		System.out.println("forward: Using default forward");
-		_dispatch(request, response);
-	}
+    public void forward(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("forward: Using default forward");
+        _dispatch(request, response);
+        }
 
-	protected void _dispatch(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException {
-		System.out.println("_dispatch: redirecting to " + getURL());
-		RequestDispatcher rd=request.getRequestDispatcher(getURL());
-		if (rd==null) System.out.println("_dispatch: rd = null!");
-		rd.forward(request,response);
-	}
+    protected void _dispatch(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("_dispatch: redirecting to " + getURL());
+        RequestDispatcher rd = request.getRequestDispatcher(getURL());
+        if (rd == null) {
+            System.out.println("_dispatch: rd = null!");
+        }
+        rd.forward(request, response);
+    }
 }
