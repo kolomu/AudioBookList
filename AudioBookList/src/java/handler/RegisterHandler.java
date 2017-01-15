623a0cbcbb1e28bbfcb38ea14c0ffd035ef0f6b1
kolomu <kevin-olomu@web.de>
2017-01-15 19:26:28 +0100

    formatted if/else

--- AudioBookList/src/java/handler/RegisterHandler.java
+++ AudioBookList/src/java/handler/RegisterHandler.java
@@ -14,72 +14,81 @@ import standard.StringUtils;
 public class RegisterHandler extends EventHandlerBase {
 
     private boolean hasErrors = false;
-    
+
     @Override
     protected String getURL() {
-        if (hasErrors) return "/pages/register.jsp";
-        else return "/pages/overview.jsp";
+        if (hasErrors) {
+            return "/pages/register.jsp";
+        } else {
+            return "/pages/overview.jsp";
+        }
     }
-    
+
     @Override
-   public void process(ServletContext sc, HttpServletRequest request, HttpServletResponse response)
-			throws Exception, ServletException, IOException {
+    public void process(ServletContext sc, HttpServletRequest request, HttpServletResponse response)
+            throws Exception, ServletException, IOException {
 
-		HashMap<String, String> errorMessages = new HashMap<>();
+        HashMap<String, String> errorMessages = new HashMap<>();
 
-		PersonDao dao = new PersonDao();
-		Person person = new Person();
-		person.initDefaultValues();
+        PersonDao dao = new PersonDao();
+        Person person = new Person();
+        person.initDefaultValues();
 
-		// get parameters from the request
-		String username = request.getParameter("username");
-		String email = request.getParameter("email");
-		String password = request.getParameter("password");
-		// String termsofuse = request.getParameter("termsofuse");
+        // get parameters from the request
+        String username = request.getParameter("username");
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        String termsofuse = request.getParameter("termsofuse");
 
-		hasErrors = false;
+        hasErrors = false;
 
-		// check Data
-                // USERNAME
-                if(StringUtils.isNullOrEmpty(username)){
-                   errorMessages.put("username" , "Username is missing"); 
-                }else {
-                   person.setUsername(username);
-                }
-                
-                // EMAIL
-                if(StringUtils.isNullOrEmpty(email)){
-                   errorMessages.put("email" , "Email is missing"); 
-                }else {
-                   person.setEmail(email);
-                }
-               
-                // PASSSWORRD
-                if(StringUtils.isNullOrEmpty(password)){
-                   errorMessages.put("password" , "Password is missing"); 
-                }else {
-                   person.setPassword(password);
-                }
-                 
-		// is username already registered
-		try {
-			@SuppressWarnings("unused")
-			Person tmp = dao.searchPerson(username);
-			errorMessages.put("username", "Username already in use");
-			
-		} catch (NoResultException nre) {
-			person.setUsername(username);
-		}
-		
-		hasErrors = !errorMessages.isEmpty();
-		
-		if (!hasErrors) {
-			dao.create(person);
-			request.setAttribute("user", person);	
-		} else {		
-			request.setAttribute("errorMessages", errorMessages);
-			request.setAttribute("user", person);
-		}
-	}
-    
+        // check Data
+        // USERNAME
+        if (StringUtils.isNullOrEmpty(username)) {
+            errorMessages.put("username", "Username is missing");
+        } else {
+            person.setUsername(username);
+        }
+
+        // EMAIL
+        if (StringUtils.isNullOrEmpty(email)) {
+            errorMessages.put("email", "Email is missing");
+        } else {
+            person.setEmail(email);
+        }
+
+        // PASSSWORRD
+        if (StringUtils.isNullOrEmpty(password)) {
+            errorMessages.put("password", "Password is missing");
+        } else if (password.length() < 6) {
+            errorMessages.put("password", "Password must have a length of atleast 6 characters");
+        } else {
+            person.setPassword(password);
+        }
+
+        if (StringUtils.isNullOrEmpty(termsofuse)) {
+            errorMessages.put("termsofuse", "You didn't agree to the Terms of Use");
+        }
+
+        // is username already registered
+        try {
+            @SuppressWarnings("unused")
+            Person tmp = dao.searchPerson(username);
+            errorMessages.put("username", "Username already in use");
+
+        } catch (NoResultException nre) {
+            person.setUsername(username);
+        }
+
+        hasErrors = !errorMessages.isEmpty();
+
+        if (!hasErrors) {
+            dao.create(person);
+            request.setAttribute("user", person);
+        } else {
+            request.setAttribute("errorMessages", errorMessages);
+            request.setAttribute("user", person);
+        }
+    }
+
 }
