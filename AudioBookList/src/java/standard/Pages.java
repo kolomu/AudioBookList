99ede3bb41c478e0dbeabf1267ae6bf98fb9c971
kolomu <kevin-olomu@web.de>
2017-01-16 15:32:55 +0100

    added signedOutPage added regiisterConfirmationPage

--- AudioBookList/src/java/standard/Pages.java
+++ AudioBookList/src/java/standard/Pages.java
@@ -3,4 +3,6 @@ package standard;
 public class Pages {
     public static String registerPage = "/pages/register.jsp";
     public static String overviewPage = "/pages/overview.jsp";
+    public static String signedOutPage = "/pages/signout.jsp";
+    public static String registerConfirmationPage = "/pages/registerconfirmation.jsp";
 }
