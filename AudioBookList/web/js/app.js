1d23ca4cf46ff8cd2941ef49156ab7f2ba4df9da
kolomu <kevin-olomu@web.de>
2017-01-21 15:36:06 +0100

    jquery star rating

--- AudioBookList/web/js/app.js
+++ AudioBookList/web/js/app.js
@@ -1,1 +1,19 @@
-$(document).foundation()
+$(document).foundation();
+
+$.fn.stars = function() {
+    return $(this).each(function() {
+        // Get the value
+        var val = parseFloat($(this).html());
+        val = Math.round(val * 4) / 4; /* To round to nearest quarter */
+        // Make sure that the value is in 0 - 5 range, multiply to get width
+        var size = Math.max(0, (Math.min(5, val))) * 16;
+        // Create stars holder
+        var $span = $('<span />').width(size);
+        // Replace the numerical value with stars
+        $(this).html($span);
+    });
+};
+
+$(function() {
+    $('span.stars').stars();
+});
