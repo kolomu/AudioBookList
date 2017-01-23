27aaf6adc0bce5d9f5f366d4211981b2d79d67e1
kolomu <kevin-olomu@web.de>
2017-01-23 14:12:29 +0100

    added insert into statements

--- audiobooklist.sql
+++ audiobooklist.sql
@@ -30,6 +30,8 @@ DROP TABLE IF EXISTS audiolist;
 --
 -- Table structures
 --
+CREATE SCHEMA audiobooklist;
+
 CREATE TABLE author (
   id int(11) unsigned NOT NULL AUTO_INCREMENT,
   firstname varchar(45) NOT NULL,
@@ -88,7 +90,7 @@ COMMIT;
 CREATE TABLE audiobook (
   id int(11) unsigned NOT NULL AUTO_INCREMENT,
   person_id int(11) unsigned DEFAULT NULL,
-  author varchar(45) NOT NULL,
+  author_id int(11) unsigned NOT NULL,
   title varchar(45) NOT NULL,
   speaker_id int(11) unsigned NOT NULL,
   publisher_id int(11) unsigned DEFAULT NULL,
@@ -100,6 +102,7 @@ CREATE TABLE audiobook (
   rating int(11) DEFAULT NULL,
   votes int(11) DEFAULT NULL,
   PRIMARY KEY (id),
+  CONSTRAINT fk_author_id FOREIGN KEY (author_id) REFERENCES author (id),
   CONSTRAINT fk_category_id FOREIGN KEY (category_id) REFERENCES category (id),
   CONSTRAINT fk_lang_id FOREIGN KEY (lang_id) REFERENCES lang (id),
   CONSTRAINT fk_person_id FOREIGN KEY (person_id) REFERENCES person (id),
@@ -131,3 +134,55 @@ CREATE TABLE audiolist (
   CONSTRAINT fk_list_person_id FOREIGN KEY (person_id) REFERENCES person (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 COMMIT;
+
+
+
+--
+-- INSERT Statements
+--
+INSERT INTO category (id, name) values (null, "Comedy");
+INSERT INTO category (id, name) values (null, "Mysteries & Thrillers");
+INSERT INTO category (id, name) values (null, "Romance");
+INSERT INTO category (id, name) values (null, "Sci-Fi & Fantasy");
+INSERT INTO category (id, name) values (null, "Fiction");
+INSERT INTO category (id, name) values (null, "History");
+INSERT INTO category (id, name) values (null, "Teens");
+INSERT INTO category (id, name) values (null, "Science & Technology");
+INSERT INTO category (id, name) values (null, "Kids");
+INSERT INTO category (id, name) values (null, "Classics");
+INSERT INTO category (id, name) values (null, "Nonfiction");
+INSERT INTO category (id, name) values (null, "Bios & Memoirs");
+INSERT INTO category (id, name) values (null, "Business");
+INSERT INTO category (id, name) values (null, "Foreign Language");
+INSERT INTO category (id, name) values (null, "Self Development");
+COMMIT;
+
+INSERT INTO lang (id, name) values (null, "english");
+INSERT INTO lang (id, name) values (null, "german");
+COMMIT;
+
+INSERT INTO author (id, firstname, lastname) values (null, "John", "Grisham");
+INSERT INTO author (id, firstname, lastname) values (null, "Nicholas", "Sparks");
+INSERT INTO author (id, firstname, lastname) values (null, "Jodi", "Picoult");
+INSERT INTO author (id, firstname, lastname) values (null, "Stephenie", "Meyer");
+INSERT INTO author (id, firstname, lastname) values (null, "James", "Luceno");
+INSERT INTO author (id, firstname, lastname) values (null, "Amor", "Towles");
+COMMIT;
+
+INSERT INTO speaker (id, firstname, lastname) values (null, "Cassandra", "Campbell");
+INSERT INTO speaker (id, firstname, lastname) values (null, "Nicholas", "Sparks");
+INSERT INTO speaker (id, firstname, lastname) values (null, "Ari", "Fliakos");
+INSERT INTO speaker (id, firstname, lastname) values (null, "Audra", "McDonald");
+INSERT INTO speaker (id, firstname, lastname) values (null, "Ellen", "Archer");
+INSERT INTO speaker (id, firstname, lastname) values (null, "Jonathan", "Davis");
+INSERT INTO speaker (id, firstname, lastname) values (null, "Nicholas", "Guy Smith");
+COMMIT;
+
+INSERT INTO publisher (id, company) values (null, "Penguin Audio");
+INSERT INTO publisher (id, company) values (null, "Random House Audio");
+INSERT INTO publisher (id, company) values (null, "Little, Brown & Company");
+INSERT INTO publisher (id, company) values (null, "Hachette Book Group USA");
+COMMIT;
+
+INSERT INTO audiobook (id, person_id, author_id, title, speaker_id, publisher_id, published, category_id, duration, lang_id, rating, votes, description) 
+VALUES (null, null, 1, "The Whistler", 1, 2,'2016-10-25', 2, 790, 1, 500, 100, "We expect our judges to be honest and wise. Their integrity and impartiality are the bedrock of the entire judicial system. We trust them to ensure fair trials, to protect the rights of all litigants, to punish those who do wrong, and to oversee the orderly and efficient flow of justice. But what happens when a judge bends the law or takes a bribe?");
