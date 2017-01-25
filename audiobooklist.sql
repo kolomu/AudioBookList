3fc4b1bd09357bc7b7eb178847b53f23db222a4f
kolomu <kevin-olomu@web.de>
2017-01-25 02:11:16 +0100

    created votes table containing audiobook, user, and the rating

--- audiobooklist.sql
+++ audiobooklist.sql
@@ -75,15 +75,6 @@ CREATE TABLE speaker (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 COMMIT;
 
-CREATE TABLE joinAudiobookSpeaker (
-  audiobook_id int(11) unsigned NOT NULL,
-  speaker_id int(11) unsigned NOT NULL,
-  PRIMARY KEY (audiobook_id, speaker_id),
-  CONSTRAINT fk_audiobookspeaker_audiobook FOREIGN KEY (audiobook_id) REFERENCES audiobook (id) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT fk_audiobookspeaker_speaker FOREIGN KEY (speaker_id) REFERENCES speaker (id) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-COMMIT;
-
 CREATE TABLE publisher (
   id int(10) unsigned NOT NULL AUTO_INCREMENT,
   company varchar(45) NOT NULL,
@@ -93,24 +84,37 @@ COMMIT;
 
 CREATE TABLE audiobook (
   id int(11) unsigned NOT NULL AUTO_INCREMENT,
-  person_id int(11) unsigned DEFAULT NULL,
   author_id int(11) unsigned NOT NULL,
-  title varchar(45) NOT NULL
+  title varchar(45) NOT NULL,
   publisher_id int(11) unsigned DEFAULT NULL,
   published date DEFAULT NULL,
   description varchar(400) DEFAULT NULL,
   category_id int(11) unsigned NOT NULL,
   duration float DEFAULT NULL,
   lang_id int(11) unsigned NOT NULL,
-  rating int(11) DEFAULT NULL,
-  votes int(11) DEFAULT NULL,
   PRIMARY KEY (id),
   CONSTRAINT fk_author_id FOREIGN KEY (author_id) REFERENCES author (id),
   CONSTRAINT fk_category_id FOREIGN KEY (category_id) REFERENCES category (id),
   CONSTRAINT fk_lang_id FOREIGN KEY (lang_id) REFERENCES lang (id),
-  CONSTRAINT fk_person_id FOREIGN KEY (person_id) REFERENCES person (id),
-  CONSTRAINT fk_publisher_id FOREIGN KEY (publisher_id) REFERENCES publisher (id),
-  CONSTRAINT fk_speaker_id FOREIGN KEY (speaker_id) REFERENCES speaker (id)
+  CONSTRAINT fk_publisher_id FOREIGN KEY (publisher_id) REFERENCES publisher (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+COMMIT;
+
+CREATE TABLE vote (
+  person_id int(11) unsigned NOT NULL,
+  audiobook_id int(11) unsigned NOT NULL,
+  rating int(11) DEFAULT NULL,
+  PRIMARY KEY (person_id, audiobook_id),
+  CONSTRAINT fk_vote_person FOREIGN KEY (person_id) REFERENCES person (id) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT fk_vote_audiobook FOREIGN KEY (audiobook_id) REFERENCES audiobook (id) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE joinAudiobookSpeaker (
+  audiobook_id int(11) unsigned NOT NULL,
+  speaker_id int(11) unsigned NOT NULL,
+  PRIMARY KEY (audiobook_id, speaker_id),
+  CONSTRAINT fk_audiobookspeaker_audiobook FOREIGN KEY (audiobook_id) REFERENCES audiobook (id) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT fk_audiobookspeaker_speaker FOREIGN KEY (speaker_id) REFERENCES speaker (id) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 COMMIT;
 
@@ -184,10 +188,18 @@ INSERT INTO publisher (id, company) values (null, "Little, Brown & Company");
 INSERT INTO publisher (id, company) values (null, "Hachette Book Group USA");
 COMMIT;
 
-INSERT INTO audiobook (id, person_id, author_id, title, speaker_id, publisher_id, published, category_id, duration, lang_id, rating, votes, description) 
-VALUES (null, null, 1, "The Whistler", 1, 2,'2016-10-25', 2, 790, 1, 500, 100, "We expect our judges to be honest and wise. Their integrity and impartiality are the bedrock of the entire judicial system. We trust them to ensure fair trials, to protect the rights of all litigants, to punish those who do wrong, and to oversee the orderly and efficient flow of justice. But what happens when a judge bends the law or takes a bribe?");
+INSERT INTO audiobook (id, author_id, title, publisher_id, published, category_id, duration, lang_id, description) 
+VALUES (null, 1, "The Whistler", 2,'2016-10-25', 2, 790, 1, "We expect our judges to be honest and wise. Their integrity and impartiality are the bedrock of the entire judicial system. We trust them to ensure fair trials, to protect the rights of all litigants, to punish those who do wrong, and to oversee the orderly and efficient flow of justice. But what happens when a judge bends the law or takes a bribe?");
 COMMIT;
 
 INSERT INTO joinAudiobookSpeaker (audiobook_id, speaker_id) values (1, 1);
 INSERT INTO joinAudiobookSpeaker (audiobook_id, speaker_id) values (1, 2);
+COMMIT;
+
+INSERT INTO person (id, username, email, password, picture) values (null, "test", "test@test.de", "123456", null);
+INSERT INTO person (id, username, email, password, picture) values (null, "test2", "test2@test.de", "123456", null);
+COMMIT;
+
+INSERT INTO vote (person_id, audiobook_id, rating) values (1, 1, 5);
+INSERT INTO vote (person_id, audiobook_id, rating) values (2, 1, 5);
 COMMIT;
