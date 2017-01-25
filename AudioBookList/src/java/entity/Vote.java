cac1ddb55544820e3691760396b3aa12db9e6bca
kolomu <kevin-olomu@web.de>
2017-01-25 02:21:41 +0100

    IMP: vote entity, to store the vote for every user for each audiobook

---
+++ AudioBookList/src/java/entity/Vote.java
@@ -0,0 +1,65 @@
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "vote")
+public class Vote implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    public Vote() {
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "person_id")
+    private Person person;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "audiobook_id")
+    private Audiobook audiobook;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public Audiobook getAudiobook() {
+        return audiobook;
+    }
+
+    public void setAudiobook(Audiobook audiobook) {
+        this.audiobook = audiobook;
+    }
+
+    @Override
+    public String toString() {
+        return "Vote{" + "id=" + id + ", person=" + person + ", audiobook=" + audiobook + '}';
+    }
+
+}
