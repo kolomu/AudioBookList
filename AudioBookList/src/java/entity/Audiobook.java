ad7b35a0304655e8b75e43b0ee620e324cd1807e
kolomu <kevin-olomu@web.de>
2017-01-25 01:37:07 +0100

    entity speakers is a many-to-many relationship. Table joinAudiobookSpeakers contains the audiobook for each speaker.

--- AudioBookList/src/java/entity/Audiobook.java
+++ AudioBookList/src/java/entity/Audiobook.java
@@ -3,6 +3,7 @@ package entity;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -10,6 +11,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
@@ -27,9 +30,9 @@ public class Audiobook implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
-
-    @OneToMany
-    @JoinColumn(name = "speaker_id")
+    
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "joinAudiobookSpeaker", joinColumns = @JoinColumn(name = "audiobook_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "speaker_id", referencedColumnName = "id"))
     private List<Speaker> speakers;
 
     @OneToMany
